1da0392a4f7aa10e6cf709714ea858711360b636
tj_dibbs <oderindejames02@gmail.com>
2022-04-21 11:47:29 +0100

    Config

--- database/config.js
+++ database/config.js
@@ -1,43 +1,40 @@
 const mongoose = require("mongoose");
-const {promisify} = require("util");
-
-mongoose.connect = promisify(mongoose.connect);
 
 class ConnectDatabase {
-	constructor(url, password) {
-		this.url = url;
-		this.password = password ?? "";
-		this.count = 0;
-	}
+  constructor(url, password) {
+    this.url = url;
+    this.password = password ?? "";
+    this.count = 0;
+  }
 
-	async connect() {
-		console.log({url: this.url, password: this.password});
-		if (this.password) {
-			console.info("There is a password for the database");
-		}
+  async connect() {
+    console.log({ url: this.url, password: this.password });
+    if (this.password) {
+      console.info("There is a password for the database");
+    }
 
-		try {
-			mongoose.connect(
-				this.url,
-				{
-					useNewUrlParser: true,
-					useUnifiedTopology: true,
-				},
-				(err, info) => {
-					if (err) throw Error("Error connecting to database");
-				}
-			);
-			return {initializeConnection: "Database connected"};
-		} catch (error) {
-			console.error({error: error.message, count: this.count});
-			this.count += 1;
+    try {
+      mongoose.connect(
+        this.url,
+        {
+          useNewUrlParser: true,
+          useUnifiedTopology: true,
+        },
+        (err, info) => {
+          if (err) throw Error("Error connecting to database");
+        }
+      );
+      return { initializeConnection: "Database connected" };
+    } catch (error) {
+      console.error({ error: error.message, count: this.count });
+      this.count += 1;
 
-			if (this.count >= 5) {
-				throw Error;
-			}
-			this.connect();
-		}
-	}
+      if (this.count >= 5) {
+        throw Error;
+      }
+      this.connect();
+    }
+  }
 }
 
 module.exports = ConnectDatabase;
